6a1fa374c5a169fb760c199775d9e836274555c8
Naoki Odajima <odajima.naoki@donuts.ne.jp>
2019-06-10 15:58:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# なにこれ
+* Slackのステータスを変更するんだよぉ
+
+# どうつかうんだ
+1. `config.go`にパラメータを入力
+    * `OWMApiKey`に**OpenWeatherMap**で発行したAPIKey
+    * `SlackUserToken`に**Slack**で発行したトークン（"Bearer xoxp-..."の形で入力）
+    * `CityLon CityLat`に天気を取得したい都市の緯度経度（最も近い位置のデータが取得される）
+2. ```bash
+    go run changeSlackStatus.go config.go
+    ```
+    で実行
+3. ステータスが変わる
+    * 絵文字: `その時間の天気の絵文字`
+    * テキスト: `その時間の天気`
+
+# これから
+* 天気の対応ができていないのでそれ
+* 天気だけじゃ寂しいので他のモードも作る
+* Bot化して自動で更新できるようにする
---
+++ config.go
@@ -0,0 +1,19 @@
+package main
+
+// OpenWeatherMap API info
+
+// CityLon 都市経度
+const CityLon = "139.700338" // Tokyo Shinjuku
+// CityLat 都市緯度
+const CityLat = "35.686427"
+// OWMApiURL API URL
+const OWMApiURL = "http://api.openweathermap.org/data/2.5/weather"
+// OWMApiKey API Token. 取得する必要あり
+const OWMApiKey = ""
+
+// Slack API info
+
+// SlackAPIURL API URL
+const SlackAPIURL = "https://slack.com/api/users.profile.set"
+// SlackUserToken API Token. 取得する必要あり
+const SlackUserToken = "Bearer xoxp-"
